89b3fbced90fad1e9966aecd2bb94dc3e6e16d33
einarstensson <einar.stensson@gmail.com>
2016-03-21 15:39:44 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,23 @@
-# Enter your procedural solution here!
+def largest_prime_factor(max)
+  divisor = 2
+  number = max 
+
+  while true
+
+    if divisor > number
+      return nil
+    end
+
+    if number / divisor == 1
+      return number
+    end
+
+    if number % divisor == 0
+      number = number / divisor
+      divisor = 2
+    else
+      divisor += 1
+    end
+
+  end
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,32 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_accessor :number
+
+  def initialize(max)
+    @max = max
+    @number = find_largest_factor
+  end
+
+  def find_largest_factor
+    divisor = 2
+
+    while true
+
+      if divisor > @max
+        return nil
+      end
+
+      if @max / divisor == 1
+        return @max
+      end
+
+      if @max % divisor == 0
+        @max = @max / divisor
+        divisor = 2
+      else
+        divisor += 1
+      end
+
+    end
+  end
+
+end
